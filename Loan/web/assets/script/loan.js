7ededf2c3f1eda781e499b18a4a1f806da1f34a3
fantaros <fantaros@192.168.1.112>
2016-10-08 00:31:19 +0800

    time payment

--- Loan/web/assets/script/loan.js
+++ Loan/web/assets/script/loan.js
@@ -5,17 +5,70 @@
 (function (window, $loan, undefined){
 	var gz = 7540;//8682+3074-4216
 	
+	function copyArray(array){
+		var narray = [], i;
+		if(array != null && array.length > 0) {
+			for(i=0;i<array.length;++i) {
+				narray.push(array[i]);
+			}
+		}
+		return narray;
+	}
+	
+	function copy(obj){
+		var jsonStr = JSON.stringify(obj), nobj = JSON.parse(jsonStr);
+		return nobj;
+	}
+	
 	$loan.addTimePayment = function (info, result){
 		if(result != null && result.length > 0) {
 			var mlv = info.lv / 1200.0,
-			i, array, date, year, month, 
+			i, array, date, year, month, nresult, 
 			infoy = info.tqhksj.substr(0,4), 
 			infom = info.tqhksj.substr(4,2),
 			fxl = (+info.fx)/100.0, 
 			bjjs = (+info.tqhked) * (1-fxl), 
 			recalc = false,
 			firstre = false,
-			sydk, dic, permonth;
+			sydk, dic, dkqs = +info.dkqs,
+			oldpm = ((+info.dked) * mlv * Math.pow(mlv + 1, +(info.dkqs)))/(Math.pow(mlv + 1, +(info.dkqs)) - 1);
+			nresult = [];
+			for(i=0;i<result.length;++i) {
+				array = result[i];
+			
+					if(array != null && array.length > 0) {
+						nresult.push(copy(array));
+						date = array[1];
+						year = date.substr(0,4);
+						month = date.substr(4,2);
+						if(year == infoy && month == infom) {
+							//重新计算剩余期数
+							sydk = +array[2];
+							sydk = sydk - bjjs;
+							for(j=i+1;j<result.length;++j) {
+								dic = [];
+								dic.push(""+(j));
+								dic.push((""+year) + (month<10?"0"+(+month):""+month));
+								month = (+month) + 1;
+								if(month > 12) {
+									year = (+year) + 1;
+									month = 1;
+								}
+								dqlx = sydk * mlv;
+								dqbj = oldpm - dqlx;
+								sydk = sydk - dqbj;
+								dic.push(""+sydk);
+								dic.push(""+oldpm);
+								dic.push(""+dqbj);
+								dic.push(""+dqlx);
+								dic.push(""+(gz - oldpm));
+								nresult.push(dic);
+							}
+							return nresult;
+						}
+					}
+			}
+			
 			return result;
 		}
 	};
