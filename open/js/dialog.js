054505739ffc6367a4d0bf503e9a3dbc0d44caa8
fantaros <fantasymon@gmail.com>
2018-03-29 17:36:56 +0800

    new open

--- open/js/dialog.js
+++ open/js/dialog.js
@@ -1,16 +1,39 @@
 $(function() {
-    var mt = ($(window).height() - 456) / 2;
-    $("#listDialogContent").css("margin-top", ("" + mt) + "px");
+    var h = $(window).height();
+
+    $("#listDialogContent").css("margin-top", ("" + ((h - 456) / 2)) + "px");
+    $("#imgDialogContent").css("margin-top", ("" + ((h - 544) / 2)) + "px");
 
     $("#dialoghref").click(function() {
-        $("#headposition").click();
-        $("body").css("position", "fixed");
+        //$("#headposition").click();
+        var top = document.body.scrollTop;
+        $("#listDialog").attr("style", "top:" + top + "px;");
+        $("body").attr("style", "top: -" + top + "px; position: fixed;");
+        //document.documentElement.scrollTop = top;
         $("#listDialog").attr("open", "open").show();
     });
 
-    $("#closedialog").click(function() {
-        $("#headposition").click();
-        $("body").css("position", "static");
-        $("#listDialog").removeAttr("open").hide();
+    $(".imgFocus").click(function() {
+        //$("#headposition").click();
+        var top = document.body.scrollTop;
+        $("#imgDialog").attr("style", "top:" + top + "px;");
+        $("#imgSrcControl").attr("src", $(this).attr("src"));
+        $("body").attr("style", "top: -" + top + "px; position: fixed;");
+        //document.documentElement.scrollTop = top;
+        $("#imgDialog").attr("open", "open").show();
+    });
+
+    $(".closedialogbutton").click(function() {
+        //$("#headposition").click();
+        $("body").attr("style", "top: 0px; position: static;");
+        $(".dialogRegion").removeAttr("open").hide();
+        document.body.scrollTo(0, parseInt($("#listDialog").css("top")));
+    });
+
+    $("#imgDialogContent").click(function() {
+        //$("#headposition").click();
+        $("body").attr("style", "top: 0px; position: static;");
+        $(".dialogRegion").removeAttr("open").hide();
+        document.body.scrollTo(0, parseInt($("#imgDialog").css("top")));
     });
 });
